25f766320397fdc77ac63a9a86db2225c642fec8
sagajdak <polina.sagaydak@mail.ru>
2021-11-06 01:16:59 +0300

    add 001

---
+++ 001/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Размер массива = ");
+int N = int.Parse(Console.ReadLine());
+int[] Mass = new int[N];
+//Заполнение массива случайными числами
+int i = 0;
+
+while (i<N)
+{
+    Mass[i] = new Random().Next(-9, 10);
+    i++;
+}
+i = 0;
+while (i<N)
+{
+Console.WriteLine(Mass[i]);
+i++;
+}
+
+
